8125a6c1f22836346959c9ea0d7d7fb2fcc3f995
janipauwels-sys <janipauwels@gmail.com>
2026-07-06 11:36:13 +0000

    13. Add benford_flag field to RiskScore

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 #![no_std]
 
 use soroban_sdk::{contract, contractimpl};
-
 use soroban_sdk::{Address, Env, Symbol};
-
 use soroban_sdk::symbol_short;
 
 #[derive(Clone)]
 pub struct RiskScore {
     pub score: u32,
+    pub benford_flag: bool,
 }
